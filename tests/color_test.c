26115c80f1b632dbb462cb413d5736578f405d5d
Kaustubh Shrivastava <kaustubh1818@gmail.com>
2021-09-29 19:50:31 +0530

    FIX: COLOR TEST, TODO: MATERIAL TEST

--- tests/color_test.c
+++ tests/color_test.c
@@ -77,6 +77,100 @@ START_TEST(convert_f_u8){
 
 }END_TEST
 
+START_TEST(colorfMultiply){
+    RGBColorF in_x = {
+        .r = 0.5,
+        .g = 0.3,
+        .b = 1
+    };
+
+    RGBColorF in_y = {
+        .r = 0.6,
+        .g = 0.4,
+        .b = 2
+    };
+
+    RGBColorF exp = {
+        .r = 0.3,
+        .g = 0.12,
+        .b = 2 
+    };
+
+    RGBColorF out = colorf_multiply(in_x,in_y);
+
+    ck_assert_colorf_eq(exp, out);
+
+
+}END_TEST
+
+START_TEST(colorfAdd){
+    RGBColorF in_x = {
+        .r = 0.5,
+        .g = 0.3,
+        .b = 1
+    };
+
+    RGBColorF in_y = {
+        .r = 0.6,
+        .g = 0.4,
+        .b = 0.9
+    };
+
+    RGBColorF exp = {
+        .r = 1.0,
+        .g = 0.7,
+        .b = 1.0
+    };
+
+    RGBColorF out = colorf_add(in_x, in_y);
+    
+    ck_assert_colorf_eq(exp, out);
+    
+
+}END_TEST
+
+START_TEST(coloru8Createf){
+
+    RGBColorU8 out = coloru8_createf(0.8, 0.5, 0.2);
+
+    RGBColorU8 exp_out = {
+        .r = 204,
+        .g = 128,
+        .b = 51
+    };
+
+    ck_assert_coloru8_eq(exp_out, out);
+
+}END_TEST
+
+
+START_TEST(COLORU8CREATE){
+
+    uint8_t a = 232;
+    uint8_t b = 134;
+    uint8_t c = 178;
+
+    RGBColorU8 color = COLOR_U8CREATE(a, b, c);
+
+    RGBColorU8 exp_color = {
+        .r = 232,
+        .g = 134, 
+        .b = 178
+    };
+
+    RGBColorU8 out = COLOR_U8CREATE(0.8, 0.5, 0.2);
+
+    RGBColorU8 exp_out = {
+        .r = 204,
+        .g = 128,
+        .b = 51
+    };
+
+    ck_assert_coloru8_eq(color, exp_color);
+    ck_assert_coloru8_eq(out, exp_out);
+
+}END_TEST
+
 Suite* hr_suite(void){
     Suite *s;
     TCase *tc_core;
@@ -90,6 +184,10 @@ Suite* hr_suite(void){
     tcase_add_test(tc_core, check_f_create_color);
     tcase_add_test(tc_core, convert_u8_f);
     tcase_add_test(tc_core, convert_f_u8);
+    tcase_add_test(tc_core, colorfMultiply);
+    tcase_add_test(tc_core, colorfAdd);
+    tcase_add_test(tc_core, coloru8Createf);
+    tcase_add_test(tc_core, COLORU8CREATE);
     suite_add_tcase(s, tc_core);
 
     return s;
