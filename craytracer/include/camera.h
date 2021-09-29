d6b18acbca095a5783c451767310c92bd2800d9b
B-u-f-f <kshitij957@gmail.com>
2021-09-30 01:03:01 +0530

    Add: look at camera

--- craytracer/include/camera.h
+++ craytracer/include/camera.h
@@ -9,18 +9,24 @@
 
 typedef struct camera {
     vec3 origin;
-    CFLOAT aspect_ratio;
-    CFLOAT viewport_height;
-    CFLOAT focal_length;
+    CFLOAT aspectRatio;
+    CFLOAT focalLength;
 
-    CFLOAT viewport_width;
-    vec3 lower_left_corner;
+    CFLOAT viewportHeight;
+    CFLOAT viewportWidth;
+    CFLOAT verticalFOV;
+    vec3 lowerLeftCorner;
     vec3 horizontal;
     vec3 vertical;
-
 } Camera;
 
-extern void cam_setCamera(Camera * restrict c);
+extern void cam_setCamera(Camera * restrict c, vec3 origin, CFLOAT aspectRatio, 
+                          CFLOAT focalLength, CFLOAT vfov);
+
+extern void cam_setLookAtCamera(Camera * restrict c, vec3 lookFrom, 
+                                vec3 lookAt, vec3 up, CFLOAT vfov, 
+                                CFLOAT aspectRatio);
+
 extern Ray cam_getRay(const Camera * restrict cam, CFLOAT u, CFLOAT v);
 
 
--- craytracer/sources/camera.c
+++ craytracer/sources/camera.c
@@ -1,22 +1,80 @@
 #include "camera.h"
 #include <stdlib.h>
+#include <tgmath.h>
 
-void cam_setCamera(Camera * restrict c){    
-    c->viewport_width = c->aspect_ratio*c->viewport_height;
-    c->vertical.y = c->viewport_height;
-    c->horizontal.x = c->viewport_width;
-
-    c->lower_left_corner.x = c->origin.x - c->horizontal.x/2 - c->vertical.x/2 - 0.0;
-    c->lower_left_corner.y = c->origin.y - c->horizontal.y/2 - c->vertical.y/2 - 0.0;
-    c->lower_left_corner.z = c->origin.z - c->horizontal.z/2 - c->vertical.z/2 - c->focal_length; 
+static CFLOAT degreesToRadians(CFLOAT deg){
+    return (M_PI / 180.0) * deg; 
 }
 
+void cam_setCamera(Camera * restrict c, vec3 origin, CFLOAT aspectRatio, 
+                          CFLOAT focalLength, CFLOAT vfov){      
+    
+    c->origin = origin;
+    c->aspectRatio = aspectRatio;
+    c->focalLength = focalLength;
+
+    c->verticalFOV = degreesToRadians(vfov); 
+
+    c->viewportHeight = 2.0 * tanf(c->verticalFOV / 2.0); 
+    c->viewportWidth = c->aspectRatio * c->viewportHeight;
+
+    c->vertical.y = c->viewportHeight;
+    c->horizontal.x = c->viewportWidth;
+
+    c->lowerLeftCorner.x = c->origin.x - c->horizontal.x/2 - c->vertical.x/2 - 0.0;
+    c->lowerLeftCorner.y = c->origin.y - c->horizontal.y/2 - c->vertical.y/2 - 0.0;
+    c->lowerLeftCorner.z = c->origin.z - c->horizontal.z/2 - c->vertical.z/2 - c->focalLength; 
+}
+
+void cam_setLookAtCamera(Camera * restrict c, vec3 lookFrom, 
+                         vec3 lookAt, vec3 up, CFLOAT vfov, 
+                         CFLOAT aspectRatio){
+
+    c->aspectRatio = aspectRatio;
+
+    c->verticalFOV = degreesToRadians(vfov); 
+
+    c->viewportHeight = 2.0 * tanf(c->verticalFOV / 2.0); 
+    c->viewportWidth = c->aspectRatio * c->viewportHeight;
+    
+    // w = dir(lookFrom - lookAt)
+    vec3 w = lookFrom;
+    vector3_subtract(&w, &lookAt);
+    vector3_normalize(&w);
+    
+    // u = dir(up - w)
+    vec3 u;
+    vector3_cross_product(&u, &up, &w);
+    vector3_normalize(&u);
+    
+    // v = cross (w, u)
+    vec3 v;
+    vector3_cross_product(&v, &w, &u);
+    
+    //origin = lookFrom 
+    c->origin = lookFrom;
+
+    // horizontal = viewport_width * u
+    c->horizontal = u;
+    vector3_multiplyf(&c->horizontal, c->viewportWidth);
+    
+    // vertical = viewport_height * v
+    c->vertical = v;
+    vector3_multiplyf(&c->vertical, c->viewportHeight);
+    
+    // lower_left_corner = origin - (horizontal + vertical)/2 - w
+    c->lowerLeftCorner = c->horizontal;
+    vector3_add(&c->lowerLeftCorner, &c->vertical);
+    vector3_multiplyf(&c->lowerLeftCorner, -0.5);
+    vector3_subtract(&c->lowerLeftCorner, &w);
+    vector3_add(&c->lowerLeftCorner, &c->origin);
+}
 
 Ray cam_getRay(const Camera * restrict cam, CFLOAT u, CFLOAT v){
     vec3 direction = {
-        .x = cam->lower_left_corner.x + u * cam->horizontal.x + v * cam->vertical.x - cam->origin.x,
-        .y = cam->lower_left_corner.y + u * cam->horizontal.y + v * cam->vertical.y - cam->origin.y,
-        .z = cam->lower_left_corner.z + u * cam->horizontal.z + v * cam->vertical.z - cam->origin.z
+        .x = cam->lowerLeftCorner.x + u * cam->horizontal.x + v * cam->vertical.x - cam->origin.x,
+        .y = cam->lowerLeftCorner.y + u * cam->horizontal.y + v * cam->vertical.y - cam->origin.y,
+        .z = cam->lowerLeftCorner.z + u * cam->horizontal.z + v * cam->vertical.z - cam->origin.z
     };
     
     return ray_create(cam->origin, direction);
